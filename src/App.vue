2958913dcd1344a7f6b9deee90e1a770863f1ea9
azmrfii <azzamrafizafran@gmail.com>
2023-09-21 22:19:35 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-4">
+    <table class="table table-bordered mt-4">
+      <thead class="thead-light">
+        <tr>
+          <th width="10%">Product Name</th>
+          <th width="10%">Price</th>
+          <th width="10%">Qty</th>
+          <th width="10%">Total</th>
+          <th width="150">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <span v-if="editIndex !== index">{{ item.name }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" v-model="item.name">
+            </span>
+          </td>
+
+          <td>
+            <span v-if="editIndex !== index">{{ item.price }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" type="number" v-model.number="item.price">
+            </span>
+          </td>
+
+          <td>
+            <span v-if="editIndex !== index">{{ item.qty }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" type="number" v-model.number="item.qty">
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
